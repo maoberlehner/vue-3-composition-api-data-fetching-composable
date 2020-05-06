82896296021d421c180020b73f03ec5a987a32f2
Markus Oberlehner <markus.oberlehner@gmail.com>
2020-05-06 18:54:20 +0200

    Add JSDdoc types

--- src/composables/swr-cache.js
+++ src/composables/swr-cache.js
@@ -5,6 +5,22 @@ import { asArray } from '../utils/as-array';
 
 const CACHE = new LRU({ max: 256 });
 
+/**
+ * @typedef {Object} SwrCacheOptions
+ * @property {number} dedupingInterval Dedupe calls with the same signature in this time span in ms.
+ */
+
+/**
+ * @typedef {Object} SwrCacheResponse
+ * @property {any} data The (resolved) return value of the callback.
+ * @property {Error} error Error thrown by the callback.
+ * @property {Function} reload Function to manually call the callback function again.
+ * @property {Symbol} state Current state in the lifecycle.
+ */
+
+/**
+ * @type SwrCacheOptions
+ */
 const DEFAULT_OPTIONS = {
   dedupingInterval: 2000,
 };
@@ -16,7 +32,17 @@ export const STATE = {
   revalidating: Symbol(`revalidating`),
 };
 
+/**
+ * A composable for caching expensive work (like data fetching) using the
+ * stale-while-revalidate invalidation strategy.
+ * @param {any} parameter A single parameter or an array of parameters to be passed to the callback.
+ * @param {Function} callback A callback function which typically returns a promise.
+ * @param {SwrCacheOptions} [customOptions] Custom configuration options.
+ */
 export function useSwrCache(parameter, callback, customOptions) {
+  /**
+   * @type SwrCacheOptions
+   */
   const options = {
     ...DEFAULT_OPTIONS,
     ...customOptions,
@@ -25,6 +51,10 @@ export function useSwrCache(parameter, callback, customOptions) {
   const parameters = asArray(parameter);
   const cacheKey = JSON.stringify(parameters);
   const cacheKeyDedupe = `${cacheKey}_dedupe`;
+
+  /**
+   * @type SwrCacheResponse
+   */
   const response = reactive({
     data: null,
     error: null,
