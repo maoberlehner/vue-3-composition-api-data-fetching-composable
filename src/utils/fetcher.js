cc3ef176dad1dade075de8aacdd3e2e96673e538
Markus Oberlehner <markus.oberlehner@gmail.com>
2020-05-09 22:04:06 +0200

    Use fake API data

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,5 +1,33 @@
-export const fetcher = endpoint => fetch(endpoint).then(response => new Promise((resolve) => {
+let count = 0;
+
+const responses = [
+  {
+    company: { name: `Microsoft` },
+    email: `john@microsoft.com`,
+    name: `John Smith`,
+    website: `https://microsoft.com/john`,
+  },
+  {
+    company: { name: `Apple` },
+    email: `john@apple.com`,
+    name: `John Smith`,
+    website: `https://apple.com/john`,
+  },
+  {
+    company: { name: `Amazon` },
+    email: `john@amazon.com`,
+    name: `John Smith`,
+    website: `https://amazon.com/john`,
+  },
+];
+
+export const fetcher = () => new Promise((resolve) => {
   setTimeout(() => {
-    resolve(response.json());
-  }, 3000);
-}));
+    resolve(responses[count]);
+    if (count === 2) {
+      count = 0;
+      return;
+    }
+    count += 1;
+  }, 2000);
+});
